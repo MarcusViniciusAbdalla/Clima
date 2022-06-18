0aa57ba8dd25adbf285fa7f1463a6a9eec76bed3
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-18 18:52:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,12 @@ function showInfo(json) {
     document.querySelector('.resultado').style.display = 'block';
 
     document.querySelector('.titulo').innerHTML = `${json.name} , ${json.country}`;
-    
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    document.querySelector('.temp img').setAttribute('src' , `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png` );
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle}deg)`;
+
 }
 
 
