a58a33990c7779ed36440761b5d3c9bd664b8c10
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-18 18:17:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-document.querySelector('.busca'),addEventListener('submit' , (event)=>{
+document.querySelector('.busca'),addEventListener('submit' , async (event)=>{
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value ;
 
-})
+    if(input !== '') {
+        showWarning('Carregando...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=a9055819d5c625bfb1eaf69389afec70&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200){
+
+        }else {
+            showWarning('Não encontramos esta localização')
+        }
+
+    }
+
+});
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML= msg;
+}
