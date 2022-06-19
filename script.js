d6e79b3a38a374e7582f5b909bba72259b352d6a
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-06-19 10:39:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.querySelector('.busca'),addEventListener('submit' , async (event)=>{
         let json = await results.json();
 
         if(json.cod === 200){
+            clearInfo();
             showInfo({
                 name: json.name,
                 country: json.sys.country,
@@ -21,25 +22,32 @@ document.querySelector('.busca'),addEventListener('submit' , async (event)=>{
                 windAngle: json.wind.deg
             });
         }else {
+            clearInfo();
             showWarning('Não encontramos esta localização')
         }
 
+    }else{
+        clearInfo();
     }
 
 });
 
 function showInfo(json) {
-    showWarning('');
-
-    document.querySelector('.resultado').style.display = 'block';
+    showWarning('');    
 
     document.querySelector('.titulo').innerHTML = `${json.name} , ${json.country}`;
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
     document.querySelector('.temp img').setAttribute('src' , `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png` );
 
-    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle}deg)`;
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
 
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
 }
 
 
